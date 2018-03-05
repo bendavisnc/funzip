c891f58b229dff11f680b8dbdf72309a91a534f7
Ben <bendavisnc@gmail.com>
2018-03-04 20:08:03 -0500

    found a mistake at map

--- src/funzip/move_result.clj
+++ src/funzip/move_result.clj
@@ -1,22 +1,50 @@
 (ns funzip.move-result
-  (:refer-clojure :exclude [map, get]))
+  (:refer-clojure :exclude [map, get])
+  (:require [clojure.spec.alpha :as spec]
+            [funzip.zipper :refer [zipper?]]))
 
 (def fail ::fail)
 (def success ::success)
 
 (defrecord MoveResult [zipper, origin, result])
 
+(defn move-result? [m]
+  (instance? MoveResult m))
+
+(spec/fdef ->MoveResult
+           :args (spec/cat :zipper (spec/nilable zipper?), :origin zipper?, :result #{fail, success})
+           :ret move-result?)
+
 (defn successful-move [& {:keys [zipper, origin]}]
-  (new MoveResult zipper, origin, success))
+  (->MoveResult zipper, origin, success))
+
+(spec/def ::zipper zipper?)
+(spec/def ::origin zipper?)
+
+;(spec/fdef successful-move
+;           :args (spec/cat :kwargs (spec/keys* :req-un [::zipper, ::origin]))
+;           :ret move-result?)
 
 (defn failed-move [& {:keys [origin]}]
-  (new MoveResult nil, origin, fail))
+  (->MoveResult nil, origin, fail))
+
+(spec/fdef failed-move
+           :args (spec/cat :kwargs (spec/keys* :req-un [::origin]))
+           :ret move-result?)
 
 (defn success? [m]
   (= (:result m) success))
 
+(spec/fdef success?
+           :args (spec/cat :m move-result?)
+           :ret boolean?)
+
 (defn fail? [m]
   (= (:result m) fail))
+
+(spec/fdef fail?
+           :args (spec/cat :m move-result?)
+           :ret boolean?)
 
 (defn get [m]
   (cond
@@ -25,13 +53,22 @@
     (success? m)
     (:zipper m)))
 
+(spec/fdef get
+           :args (spec/cat :m move-result?)
+           :ret (spec/nilable zipper?))
+
 (defn map [m, f]
   (cond
     (fail? m)
     nil
     (success? m)
     (successful-move :zipper (f (:zipper m))
-                     :origin m)))
+                     :origin (:origin m))))
+
+(spec/fdef map
+           :args (spec/cat :m move-result?, :f fn?)
+           :ret (spec/nilable move-result?))
+
 
 (defn with-origin [m, origin]
   (cond
@@ -41,5 +78,8 @@
     (successful-move :zipper (:zipper m)
                      :origin origin)))
 
+(spec/fdef with-origin
+           :args (spec/cat :m move-result?, :origin zipper?)
+           :ret move-result?)
 
 
