6fbd0f4f95ccaca421e20990e511d52b5a50acc4
Ben <bendavisnc@gmail.com>
2018-03-14 13:32:43 -0400

    added doc to protocols

--- src/funzip/protocols.clj
+++ src/funzip/protocols.clj
@@ -1,11 +1,20 @@
 (ns funzip.protocols
+  "Defines the two relevant open protocols.
+
+   `CanBeZipper` is the main protocol that lets a data structure be a zipper.
+   `CanConvertZipper` is currently only necessary for funzip.core/into to work.
+  "
   (:import (clojure.lang IPersistentVector)))
 
 (defprotocol CanBeZipper
-  (unzip [this])
-  (zip [this, children]))
+  "Defines all of the methods needed to support zipper functionality."
+  (unzip [this]
+    "Gets the children of this node.")
+  (zip [this, children]
+    "Sets the children of this node."))
 
 (defprotocol CanConvertZipper
+  "Defines a node getter and setter method for funzip.core/into to work."
   (node [this], [this, v]))
 
 (extend-type IPersistentVector
